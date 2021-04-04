c4a694d1c5e78fdb5307c32af4b43862eef0d623
Jaroslaw Zabiello <hipertracker@gmail.com>
2021-04-04 02:36:33 +0100

    fix overmind configuration

--- src/overmind/state.ts
+++ src/overmind/state.ts
@@ -3,9 +3,7 @@ type State = {
   counter: number;
 };
 
-const state: State = {
+export const state: State = {
   welcome: "Vue 3 + TypeScript + Overmind (the best UI state lib)",
   counter: 0,
 };
-
-export default state;
