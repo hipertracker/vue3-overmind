0d3be0e1127a7d5df8cd7bad637233668b3d3b02
Jaroslaw Zabiello <hipertracker@gmail.com>
2021-06-20 03:07:16 +0100

    upgrade packages including vue 3.0 -> 3.1.1

--- src/components/IncBtn.vue
+++ src/components/IncBtn.vue
@@ -1,7 +1,18 @@
 <template>
   <button
     type="button"
-    class="inline-flex items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    class="
+      inline-flex
+      items-center
+      p-1.5
+      border border-transparent
+      rounded-full
+      shadow-sm
+      text-white
+      bg-indigo-600
+      hover:bg-indigo-700
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+    "
   >
     <svg
       class="h-5 w-5"
